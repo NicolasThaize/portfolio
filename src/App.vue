11ae3212054a3fe6ebc2af135ccc0c23bf830fe5
NicolasThaize <NicolasThaize>
2020-12-05 16:51:44 +0100

    First commit of the project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">
+        <button class="button is-primary">Home</button>
+      </router-link>
+      |
+      <router-link to="/about">
+        <button class="button is-primary">About</button>
+      </router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+</style>
