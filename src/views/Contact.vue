ec7d65415cb7f242b745b95a2df401a4352e9751
NicolasThaize <NicolasThaize>
2020-12-06 12:34:58 +0100

    Udpating all links to router-link OK

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,69 @@
 <template>
+  <div>
+    <Header></Header>
+    <section class="bgBlank hero mainContent">
+      <nav class="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+        <ul>
+          <li><router-link to="/"><a>NicolasThaize</a></router-link></li>
+          <li class="is-active"><a href="#" aria-current="page">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="container columns marginContact">
+        <div class="column">
+          <h1 class="is-family-secondary has-text-black-bis has-text-weight-bold is-size-1">CONTACT ME.</h1>
+          <p class="is-size-7 has-text-grey-light">All fields are required.</p>
+          <div>
+            <form method="post" action="#">
+              <div class="field">
+                <label class="label" for="name">Your name</label>
+                <div class="control">
+                  <input class="input" type="text" id="name">
+                </div>
+              </div>
 
+              <div class="field">
+                <label class="label" for="email">Your Email</label>
+                <div class="control has-icons-left has-icons-right">
+                  <input class="input" type="email" id="email">
+                  <span class="icon is-small is-left centerLetter">
+                            <i class="fas fa-envelope center"></i>
+                        </span>
+                </div>
+                <p class="help is-danger"></p>
+              </div>
+
+              <div class="field">
+                <label class="label" for="message">Your message</label>
+                <div class="control">
+                  <textarea class="textarea" id="message"></textarea>
+                </div>
+              </div>
+
+              <div class="control right">
+                <button class="button styleButton" type="submit" disabled>Submit (soon)</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="column">
+          <p>
+            If you want to contact me for work or any questions, you are free to fill the form beside or sending
+            me an email thanks to the button below.
+          </p>
+          <a href="mailto:nico.thaize@outlook.fr"><button class="button styleButton marginContact">nico.thaize@outlook.fr</button></a>
+        </div>
+      </div>
+    </section>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
-  name: "contact"
+  name: "contact",
+  components: {Footer, Header}
 }
 </script>
 
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,13 +1,155 @@
 <template>
+  <div>
+    <Header></Header>
+    <section class="bgBlank hero mainContent">
+      <nav class="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+        <ul>
+          <li><router-link to="/"><a>NicolasThaize</a></router-link></li>
+          <li class="is-active"><a href="#" aria-current="page">Skills</a></li>
+        </ul>
+      </nav>
+      <div class="hero-body">
+        <p class="center marginBottom20">Here is all my knowledges in computing, i know how to fully use all the technologies below.</p>
+        <!-- DESKTOP -->
 
+        <div class="columns is-marginless center is-hidden-touch">
+          <div class="left column">
+            <div class="container has-text-centered has-background-white-ter cornerRadius" style="width: 300px; box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);">
+              <div class="marginBottom20">
+                <img src="../assets/img/logiciel.png" alt="logo logiciel" class="resizeLogos">
+                <p class="has-text-weight-bold is-family-secondary">SOFTWARE DEVELOPMENT</p>
+              </div>
+              <div>
+                <p>Python</p>
+                <p>C</p>
+                <p>C++</p>
+                <p>Java</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="center column">
+            <div class="container has-text-centered has-background-white-ter cornerRadius" style="width: 300px; box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);">
+              <div class="marginBottom20">
+                <img src="../assets/img/web.png" alt="logo web" class="resizeLogos">
+                <p class="has-text-weight-bold is-family-secondary">WEB DEVELOPMENT</p>
+              </div>
+              <div>
+                <p>HTML / CSS / PHP</p>
+                <p>Javascript / Jquery</p>
+                <p>Bulma</p>
+                <p>Bootstrap</p>
+                <p>WordPress</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="right column">
+            <div class="container has-text-centered has-background-white-ter cornerRadius" style="width: 300px; box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);">
+              <div class="marginBottom20">
+                <img src="../assets/img/infra.png" alt="logo infra" class="resizeLogos">
+                <p class="has-text-weight-bold is-family-secondary">IT INFRASTRUCTURE</p>
+              </div>
+              <div>
+                <p>SQL</p>
+                <p>Linux (debian)</p>
+                <p>Windows server</p>
+                <p>Apache</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- MOBILE -->
+
+      <div class="is-hidden-desktop is-marginless forceMB">
+        <div class="center column">
+          <div class="container has-text-centered has-background-white-ter cornerRadius" style="width: 300px; box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);">
+            <div class="marginBottom20">
+              <img src="../assets/img/web.png" alt="logo web" class="resizeLogos">
+              <p class="has-text-weight-bold is-family-secondary">WEB DEVELOPMENT</p>
+            </div>
+            <div>
+              <p>HTML / CSS / PHP</p>
+              <p>Javascript / Jquery</p>
+              <p>Bulma</p>
+              <p>Bootstrap</p>
+              <p>WordPress</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="left column">
+          <div class="container has-text-centered has-background-white-ter cornerRadius" style="width: 300px; box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);">
+            <div class="marginBottom20">
+              <img src="../assets/img/logiciel.png" alt="logo logiciel" class="resizeLogos">
+              <p class="has-text-weight-bold is-family-secondary">SOFTWARE DEVELOPMENT</p>
+            </div>
+            <div>
+              <p>Python</p>
+              <p>C</p>
+              <p>C++</p>
+              <p>Java</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="right column">
+          <div class="container has-text-centered has-background-white-ter cornerRadius" style="width: 300px; box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);">
+            <div class="marginBottom20">
+              <img src="../assets/img/infra.png" alt="logo infra" class="resizeLogos">
+              <p class="has-text-weight-bold is-family-secondary">IT INFRASTRUCTURE</p>
+            </div>
+            <div>
+              <p>SQL</p>
+              <p>Linux (debian)</p>
+              <p>Windows server</p>
+              <p>Apache</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="hero-foot columns">
+        <div class="left desktop column">
+          <router-link to="/works"><a class="marginLinks">
+            <div>
+              <p>My works</p>
+            </div>
+            <div class="left">
+              <i class="fas fa-chevron-left animArrowLeft"></i>
+            </div>
+          </a></router-link>
+        </div>
+        <div class="right desktop column">
+          <router-link to="/contact"><a class="marginLinks">
+            <div>
+              <p>Contact</p>
+            </div>
+            <div class="right">
+              <i class="fas fa-chevron-right animArrowRight"></i>
+            </div>
+          </a></router-link>
+        </div>
+      </div>
+    </section>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
-name: "skills"
+  name: "skills",
+  components: {Footer, Header},
+  metaInfo: {
+    title: "My Skills - Nicolas THAIZE"
+  }
 }
 </script>
 
 <style scoped>
-
+@import "../assets/css/skills.css";
 </style>
